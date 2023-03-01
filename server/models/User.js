23bebb49b810aecca301aae526b2a25cd4e8dab1
Ken Kaiser <kenkaiser19@gmail.com>
2023-02-28 19:25:15 -0700

    added in starting models

---
+++ server/models/User.js
@@ -0,0 +1,21 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Must match an email address!'],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
---
+++ server/models/index.js
@@ -0,0 +1,4 @@
+const User = require('./User');
+const Budget = require('./Budget');
+
+module.exports = { User, Budget };
