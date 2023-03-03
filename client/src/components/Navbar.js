4213bf37709aa6e42d5a88b44cfc8ac498276ecd
FruityOkapi <josephbsimmerman@gmail.com>
2023-03-03 00:30:10 -0500

    working on Login and Register modals

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Typography, Menu} from 'antd';
-import {HomeOutlined, MehOutlined} from '@ant-design/icons'
-import { useState } from 'react';
+import {HomeOutlined, MehOutlined, LoginOutlined, BookOutlined} from '@ant-design/icons'
+import {useState} from 'react'
 
 export default function Nav() {
  //const linkStyle = { border: '1px black', padding: '5px' };
@@ -16,10 +16,19 @@ const navItems = [
     key: 'profile',
     icon: <MehOutlined />,
   },
-  
+  {
+    label: 'Login',
+    key: 'login',
+    icon: <LoginOutlined />
+  },
+  {
+    label: 'Register',
+    key: 'register',
+    icon: <BookOutlined />
+  }
 ]
 
-const [current, setCurrent] = useState('home');
+const [current, setCurrent] = useState('mail');
 const onClick = (e) => {
   console.log('click ', e);
   setCurrent(e.key);
