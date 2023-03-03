a2d8bc576d516533f66a958b9221a8847bb46a92
FruityOkapi <josephbsimmerman@gmail.com>
2023-03-02 21:07:15 -0500

    added html and css to app.js,home.js,navbar.js and fixed npm installation dirs

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import {
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {Layout, Col, Divider, Row, Typography, List} from 'antd'
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import AddBudget from './components/AddBudget';
@@ -40,43 +41,49 @@ const client = new ApolloClient({
   cache: new InMemoryCache(),
 });
 
+const {Header, Content, Footer} = Layout;
+const {Text, Link} = Typography;
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
-          <Header />
-          <div className="container">
-            <Routes>
+          <Header style={{
+            background: '#36cfc9'
+          }}>
+            <Navbar/>
+          </Header>
+          <Routes>
               <Route 
                 path="/" 
                 element={<Home />} 
               />
               <Route 
-                path="/login" 
-                element={<Login />} 
-              />
-              <Route 
                 path="/Profile" 
                 element={<Profile />} 
               />
-              <Route 
-                path="/AddBudget" 
-                element={<AddBudget />} 
-              />
-              <Route 
-                path="/Navbar" 
-                element={<Navbar />} 
-              />
-              <Route 
-              path="/Signup" 
-              element={<Signup />} 
-            />
-            </Routes>
-          </div>
-          <Footer />
-        </div>
+          </Routes>
+          <Footer
+          style={{
+            background: "#b5f5ec"
+          }}>
+            <Row justify="space-between" align="top">
+              <Col span={6}>
+                <Divider orientation='left'>Our Githubs</Divider>
+                <List bordered={false}>
+                  <List.Item><Link href='https://github.com/jaspersonja' target="_blank">Jacob</Link></List.Item>
+                  <List.Item><Link href='https://github.com/Ashe-Radus' target="_blank">Ashe</Link></List.Item>
+                  <List.Item><Link href='https://github.com/Javier2596' target="_blank">Javier</Link></List.Item>
+                  <List.Item><Link href='https://github.com/kkaiser101' target="_blank">Ken</Link></List.Item>
+                  <List.Item><Link href='https://github.com/FruityOkapi' target="_blank">Joseph</Link></List.Item>
+                </List>
+              </Col>
+              <Col span={10}>
+                <Divider orientation='left'>About Us</Divider>
+                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+              </Col>
+            </Row>
+          </Footer>
       </Router>
     </ApolloProvider>
   );
