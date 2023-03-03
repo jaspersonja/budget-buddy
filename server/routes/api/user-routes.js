bba67e356885a5d2de2749f83e7cee85fafa6ce0
unknown <jacob.jasperson@yahoo.com>
2023-03-02 20:44:27 -0700

    routes done except delete route

---
+++ server/routes/api/user-routes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const {
+  createUser,
+  getSingleUser,
+  saveBudget,
+  deleteBudget,
+  login,
+} = require('../../controllers/user-controller');
+
+// import middleware
+const { authMiddleware } = require('../../utils/auth');
+
+// put authMiddleware anywhere we need to send a token for verification of user
+router.route('/').post(createUser).put(authMiddleware, saveBudget);
+
+router.route('/login').post(login);
+
+router.route('/me').get(authMiddleware, getSingleUser);
+//delete route needs work. Would we need something like :incomeId , :billsId, :shoppingId? Or just simply :budgetId
+router.route('/budget/').delete(authMiddleware, deleteBudget);
+
+module.exports = router;
