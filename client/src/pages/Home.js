813d62c6e44563040b091da6251a8e6ce2af46d0
FruityOkapi <josephbsimmerman@gmail.com>
2023-03-06 20:59:49 -0500

    temp commit

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import {Layout,} from 'antd';
+import React, {useState} from 'react';
+import {Layout, Button} from 'antd';
 import NotLoggedHome from '../components/NotLoggedHome';
+import LoggedInHome from '../components/LoggedInHome';
 const {Content} = Layout;
 
 const Home = () => {
-
-    
+    // Adding logic to set loggedIn manually without being logged in. Replace with actual logged in logic.
+    const [loggedIn, setLoggedIn] = useState(false);
+    const click = () => {
+        switch(loggedIn) {
+            case true:
+                setLoggedIn(false);
+                break;
+            case false:
+                setLoggedIn(true)
+        }
+    }
     return (
         <Content
         style={{
             padding: '10px 50px',
-            height: '600px',
+            height: "calc(100vh - 200px)",
             background: '#e6fffb'
           }}>
-            <NotLoggedHome/>
+            {loggedIn ? <LoggedInHome/> : <NotLoggedHome/>}
+            {/* Temporary! REMOVE BUTTON WHEN LOGIN LOGIC IS ADDED*/}
+            <Button onClick={click}>{loggedIn ? 'Logout' : 'Login'}</Button>
         </Content>
     )
 }
