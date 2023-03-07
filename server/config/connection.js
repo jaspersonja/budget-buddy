6b9157f4e6d6f224dab5d3f3dae9f703cb76df98
unknown <jacob.jasperson@yahoo.com>
2023-03-06 19:09:58 -0700

    pushing to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 //TODO: set up the proper path mongodb://localhost/<db-name-here>
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budgetdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
